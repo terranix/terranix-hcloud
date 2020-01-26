999eafbb7e497c9c9097f3d0a1dc4a577f50b538
Ingolf Wagner <contact@ingolf-wagner.de>
2020-01-26 21:33:46 +1300

    import provider.nix from core module and refactoring

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, ... }:
 {
   imports = [
+    ./module/provider.nix
     ./module/server.nix
     ./module/nixserver.nix
     ./module/export.nix
--- module/nixserver.nix
+++ module/nixserver.nix
@@ -11,101 +11,97 @@ let
 
 in {
 
-
   options.hcloud.nixserver = mkOption {
-    default = {};
-    type = with types; attrsOf (submodule ({ name, ... }: {
-      options = {
-        enable = mkEnableOption "nixserver";
+    default = { };
+    description = ''
+      create a nixos server, via nixos-infect.
+    '';
+    type = with types;
+      attrsOf (submodule ({ name, ... }: {
+        options = {
+          enable = mkEnableOption "nixserver";
 
-        # todo eine option für zusätzlichen speicher
-        name = mkOption {
-          default = "nixserver-${name}";
-          type = with types; str;
-          description = ''
-            name of the server running nixos
-          '';
+          # todo eine option für zusätzlichen speicher
+          name = mkOption {
+            default = "nixserver-${name}";
+            type = with types; str;
+            description = ''
+              name of the server
+            '';
+          };
+          serverType = mkOption {
+            default = "cx11";
+            type = with types; str;
+            description = ''
+              Hardware equipment.This options influences costs!
+            '';
+          };
+          channel = mkOption {
+            default = "nixos-19.09";
+            type = with types; str;
+            description = ''
+              nixos channel to install
+            '';
+          };
+          backups = mkOption {
+            default = false;
+            type = with types; bool;
+            description = ''
+              enable backups or not
+            '';
+          };
+          configurationFile = mkOption {
+            default = null;
+            type = with types; nullOr path;
+            description = ''
+              The configuration.nix,
+              only used by the initial
+              provisioning by nixos-infect.
+            '';
+          };
+          provisioners = mkOption {
+            default = [ ];
+            type = with types; listOf attrs;
+            description = ''
+              provision steps. see `hcloud.server.provisioners`.
+            '';
+          };
         };
-        serverType = mkOption {
-          default = "cx11";
-          type = with types; str;
-          description = ''
-            server type (different costs)
-          '';
-        };
-        channel = mkOption {
-          default = "nixos-19.09";
-          type = with types; str;
-          description = ''
-            nixos channel to install
-          '';
-        };
-        backups = mkOption {
-          default = false;
-          type = with types; bool;
-          description = ''
-            enable backups or not
-          '';
-        };
-        configurationFile = mkOption {
-          default = null;
-          type = with types; nullOr path;
-          description = ''
-            additional configuration File
-          '';
-        };
-        provisioners = mkOption {
-          default = [];
-          type = with types; listOf attrs;
-        };
-      };
-    }));
+      }));
   };
 
-  config = mkIf (cfg != {}){
+  config = mkIf (cfg != { }) {
 
-    hcloud.server = mapAttrs' (
-      name: configuration:
-      {
-        name = "${configuration.name}";
-        value = {
-          inherit (configuration) enable serverType backups name;
-          provisioners = [
-            {
-              file.source = "${nixosInfect}/nixos-infect";
-              file.destination = "/root/nixos-infect";
-            }
-            (
-              optionalAttrs
-              (configuration.configurationFile != null)
-              {
-                file.source = configuration.configurationFile;
-                file.destination = "/etc/nixos_input.nix";
-              }
-            )
-          ]
-          ++ configuration.provisioners ++
-          [
-            {
-              remote-exec.inline = [
-                ''
-                  NO_REBOOT="dont" \
-                  PROVIDER=HCloud \
-                  NIX_CHANNEL=${configuration.channel} \
-                  ${
-                    optionalString
-                    (configuration.configurationFile != null)
-                    "NIXOS_IMPORT=/etc/nixos_input.nix"
-                  } \
-                  bash /root/nixos-infect 2>&1 | tee /tmp/infect.log
-                ''
-                ''shutdown -r +1''
-              ];
-            }
+    hcloud.server = mapAttrs' (name: configuration: {
+      name = "${configuration.name}";
+      value = {
+        inherit (configuration) enable serverType backups name;
+        provisioners = [
+          {
+            file.source = "${nixosInfect}/nixos-infect";
+            file.destination = "/root/nixos-infect";
+          }
+          (optionalAttrs (configuration.configurationFile != null) {
+            file.source = configuration.configurationFile;
+            file.destination = "/etc/nixos_input.nix";
+          })
+        ] ++ configuration.provisioners ++ [{
+          remote-exec.inline = [
+            ''
+              NO_REBOOT="dont" \
+              PROVIDER=HCloud \
+              NIX_CHANNEL=${configuration.channel} \
+              ${
+                optionalString (configuration.configurationFile != null)
+                "NIXOS_IMPORT=/etc/nixos_input.nix"
+              } \
+              bash /root/nixos-infect 2>&1 | tee /tmp/infect.log
+            ''
+            "shutdown -r +1"
           ];
-        };
-      }
-    ) cfg;
+        }];
+      };
+    }) cfg;
   };
 
 }
